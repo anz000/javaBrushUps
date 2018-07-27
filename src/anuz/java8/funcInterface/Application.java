e6aef0b43ab195b6da73979923e97eb01f5151b9
= <diabolicrage@gmail.com>
2018-07-27 01:56:52 -0600

    Using Annonymous as well as Lamda Expression

--- src/anuz/java8/funcInterface/Application.java
+++ src/anuz/java8/funcInterface/Application.java
@@ -14,6 +14,12 @@ public class Application {
 		
 		CreditCard cc = new CreditCard(75643844, "Anuj", 144.00, LocalDateTime.of(2020, Month.JANUARY, 25, 6, 30));
 		
+
+		/**
+		 * Using Annonymous Class
+		 */
+		System.out.println(" USING ANNONYMOUS CLASS");
+		
 		System.out.println("Card Details : " + cc);
 		cc.checkCC(new CardOperation() {
 			@Override
@@ -24,8 +30,33 @@ public class Application {
 					System.out.println(" Your card with has less than $100 or more than $600 as balance ");
 				} else { // if passes both test
 					System.out.println(" Your card "+ cc.getCardNumber() +" is ready for use ");
+					System.out.println("Preparing for payment...");
+					System.out.println("Payment DONE!...");			
 				}
 			}
 		});
+		
+		System.out.println();
+		System.out.println();
+		
+		/**
+		 * Using Lamda Expression 
+		 */
+		System.out.println(" USING LAMBDA EXPRESSION");
+		
+		System.out.println("Card Details : " + cc);
+		CardOperation cOperation = () -> {
+			System.out.println("Preparing for payment...");
+			System.out.println("Payment DONE!...");
+		};
+		
+		if(!cOperation.validate(cc.getExpDate()))
+			System.out.println(" Your card has expired ");
+		else if(!CardOperation.validateCardAmount(cc.getAmount()))
+			System.out.println(" Your card with has less than $100 or more than $600 as balance ");
+		else {
+			System.out.println(" Your card "+ cc.getCardNumber() +" is ready for use ");
+			cOperation.pay();
+		}
 	}
 }
